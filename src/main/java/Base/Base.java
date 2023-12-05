03741e024e1f9ee492d3587b996e724e6f584d44
Utkarshgithub8 <152481416+Utkarshgithub8@users.noreply.github.com>
2023-12-06 00:13:48 +0530

    OrangeHRM

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -2,6 +2,7 @@ package Base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -14,7 +15,6 @@ import utils.ExtentReportManager;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
 
@@ -24,7 +24,8 @@ public class Base {
 	public ExtentReports report = ExtentReportManager.getReportInstance();
 	public ExtentTest logger;
 
-	// To call different browsers
+
+	@SuppressWarnings("deprecation")
 	public void invokeBrowser() {
 		prop = new Properties();
 
@@ -35,52 +36,52 @@ public class Base {
 			e.printStackTrace();
 		}
 
-		// To Open Chrome Browser
+		
 		if (prop.getProperty("browserName").matches("chrome")) {
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			
 			driver = new ChromeDriver();
 		}
 
-		// To Open Firefox Browser
+	
 		if (prop.getProperty("browserName").matches("firefox")) {
 			driver = new FirefoxDriver();
 		}
 
-		// To maximize the Browser Window
+	
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 
 	}
 
-	// To open the Main Page URL
+
 	public void openURL() {
 		driver.get(prop.getProperty("websiteURLKey"));
 	}
 
-	// Function to show the failed test cases in the report
+
 	public void reportFail(String report) {
 		logger.log(Status.FAIL, report);
 	}
 
-	// Function to show the passed test cases in the report
+
 	public void reportPass(String report) {
 		logger.log(Status.PASS, report);
 	}
 
 
-	public void wait(long sec, By locator) {
+	public void wait(Duration sec, By locator) {
 		wait = new WebDriverWait(driver, sec);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
 
-	// To input all data to the report
+
 	public void endReport() {
 		report.flush();
 	}
 
-	// To close the Browser
+
 	public void closeBrowser() throws IOException {
 		driver.quit();
 	}
--- src/main/java/Pages/OrangeHrm.java
+++ src/main/java/Pages/OrangeHrm.java
@@ -3,13 +3,8 @@ package Pages;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-//import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import Base.Base;
 
 public class OrangeHrm extends Base{
@@ -21,18 +16,16 @@ public class OrangeHrm extends Base{
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span")).click();
-		//Thread.sleep(2000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		WebDriverWait wait = new WebDriverWait(driver, 20);
-		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
-		//Thread.sleep(2000);
-		WebDriverWait wait1 = new WebDriverWait(driver, 20);
-		WebElement element1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='oxd-table']")));
-		//Thread.sleep(2000);
+		//WebDriverWait wait = new WebDriverWait(driver, 20);
+		//WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+		//WebDriverWait wait1 = new WebDriverWait(driver, 20);
+		//WebElement element1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='oxd-table']")));
+		Thread.sleep(2000);
 		WebElement s = driver.findElement(By.xpath("//div[@class='oxd-table']"));
-		String st = s.getText();
-		
-	    System.out.println(st);
+        String st = s.getText();
+		System.out.println(st);
 		
 	    try {
 	    	PrintStream ps = new PrintStream(new File("Output File\\DemoFile.txt"));
